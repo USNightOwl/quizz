a800d434fc04bff618e1cb0c5a9eb37d8239da3c
21120447 <haonhat2729@gmail.com>
2024-06-20 20:08:57 +0700

    fix: color pagination

--- src/components/CustomPagination.vue
+++ src/components/CustomPagination.vue
@@ -54,21 +54,20 @@
   .paginate-buttons {
     height: 40px;
     width: 40px;
-    border-radius: 20px;
     cursor: pointer;
     background-color: rgb(242, 242, 242);
-    border: 1px solid rgb(217, 217, 217);
-    color: black;
+    border-radius: 2px;
+    font-weight: 500;
+    color: #606266;
     &:hover{
       background-color: #d8d8d8;
     }
   }
   .active-page {
-    background-color: #3498db;
-    border: 1px solid #3498db;
+    background-color: #67c23a;
     color: white;
     &:hover{
-      background-color: #2988c8;
+      background-color: #70d33e;
     }
   }
 
