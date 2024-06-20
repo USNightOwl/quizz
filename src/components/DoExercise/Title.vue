96f32787779679d9024cdfa4612eb9d331dc7a25
21120447 <haonhat2729@gmail.com>
2024-06-21 01:28:00 +0700

    fix: to home size

--- src/components/DoExercise/Title.vue
+++ src/components/DoExercise/Title.vue
@@ -1,9 +1,9 @@
 <template lang="">
-  <div class="mb-5 md:mt-4 mt-2 flex md:gap-2 gap-4 items-center pl-2">
+  <div class="mb-5 md:mt-4 mt-2 flex md:gap-2 gap-4 items-center pl-1">
     <h1 class="text-2xl font-bold text-center flex-1">
       {{ props.name }}
     </h1>
-    <router-link to="/" class="w-[10%] md:w-[8%]">
+    <router-link to="/" class="w-[10%] md:w-[8%] text-center">
       <Icon icon="pi-home" className="text-2xl"/>
     </router-link>
   </div>
