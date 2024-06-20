4ceae27c018723cdf9211778c827bd3b0df7fa14
21120447 <haonhat2729@gmail.com>
2024-06-21 01:22:21 +0700

    fix: title size

--- src/components/DoExercise/Title.vue
+++ src/components/DoExercise/Title.vue
@@ -1,6 +1,6 @@
 <template lang="">
   <div class="mb-4 md:mt-4 mt-2 flex md:gap-2 gap-4 items-center pl-2">
-    <h1 class="md:text-2xl text-xl font-bold text-center flex-1">
+    <h1 class="text-2xl font-bold text-center flex-1">
       {{ props.name }}
     </h1>
     <router-link to="/" class="w-[8%] text-center">
