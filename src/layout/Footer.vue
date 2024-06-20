4abfc24d67da7eaf8503c2e4cd68a1d72c1e297c
21120447 <haonhat2729@gmail.com>
2024-06-20 20:48:38 +0700

    feat: add DMCA Protection

--- src/layout/Footer.vue
+++ src/layout/Footer.vue
@@ -1,6 +1,6 @@
 <template lang="">
   <div class="h-16 bg-slate-100 flex items-center justify-center mt-5">
-    Footer
+    <a data-v-3b694c60="" href="https://www.dmca.com/r/qr91yj7" title="DMCA.com Protection Status" class="dmca-badge"><img data-v-3b694c60="" src="https://images.dmca.com/Badges/dmca-badge-w150-5x1-01.png?ID=c1e2b7f2-44be-4475-8a87-4678509929ee" alt="DMCA.com Protection Status"></a>
   </div>
 </template>
 <script setup>
